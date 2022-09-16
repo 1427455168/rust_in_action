344186999725ff1f67dff7e0a51f797e5b3c4ca3
mjh <ma_jin_hong@foxmail.com>
2022-09-16 22:25:56 +0800

    ch5 ok

--- ch5/parse_f32/target/debug/deps/parse_f32-2a75befd7bf65312.d
+++
@@ -1,5 +0,0 @@
-/home/mjh/Labs/rust_in_action/ch5/parse_f32/target/debug/deps/parse_f32-2a75befd7bf65312: src/main.rs
-
-/home/mjh/Labs/rust_in_action/ch5/parse_f32/target/debug/deps/parse_f32-2a75befd7bf65312.d: src/main.rs
-
-src/main.rs:
--- ch5/parse_f32/target/debug/parse_f32.d
+++
@@ -1,1 +0,0 @@
-/home/mjh/Labs/rust_in_action/ch5/parse_f32/target/debug/parse_f32: /home/mjh/Labs/rust_in_action/ch5/parse_f32/src/main.rs
